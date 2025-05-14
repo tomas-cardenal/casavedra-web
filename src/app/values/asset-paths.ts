a357aa579ed87d8ddeac51570f056ee3fa3a67f3
Tomás Cardenal <tomas.car.guitv@gmail.com>
2025-05-14 11:19:07 +0200

    feat: add all pictures and declare asset paths

--- src/app/values/asset-paths.ts
+++ src/app/values/asset-paths.ts
@@ -2,3 +2,19 @@ export abstract class LogoPaths {
   static readonly LOGOS_FOLDER = 'assets/logos';
   static readonly LOGO = `${LogoPaths.LOGOS_FOLDER}/logo-new.png`;
 }
+
+export abstract class AboutSectionPaths {
+  static readonly ABOUT_FOLDER = 'assets/about';
+  static readonly ABOUT_1 = `${AboutSectionPaths.ABOUT_FOLDER}/1.jpg`;
+  static readonly ABOUT_2 = `${AboutSectionPaths.ABOUT_FOLDER}/2.jpg`;
+}
+
+export abstract class StudioSectionPaths {
+  static readonly STUDIO_FOLDER = 'assets/studio';
+  static readonly STUDIO_1 = `${StudioSectionPaths.STUDIO_FOLDER}/1.jpg`;
+  static readonly STUDIO_2 = `${StudioSectionPaths.STUDIO_FOLDER}/2.jpg`;
+}
+
+export abstract class ContactSectionPaths {
+  static readonly CONTACT_ARTWORK = 'assets/artwork/13.jpg';
+}
